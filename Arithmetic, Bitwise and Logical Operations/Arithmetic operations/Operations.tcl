a3b6a94174bcbcdabdd2c375980bdd7d4ab9763a
Irfaan <138297234+Irfaan1S@users.noreply.github.com>
2025-06-29 13:03:55 +0530

    Create Operations.tcl

---
+++ Arithmetic, Bitwise and Logical Operations/Arithmetic operations/Operations.tcl
@@ -0,0 +1,9 @@
+set a "5";
+set b "3";
+set c [expr "$a + $b"];
+set d [expr "$a - $b"];
+set e [expr "$a * $b"];
+
+puts "addition of a and b is $c";
+puts "subtraction of a and b is $d";
+puts "multiplication of a and b is $e";
