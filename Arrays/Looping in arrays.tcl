8569aca85bfe78c277a0206ca35a9308ef915118
Irfaan <138297234+Irfaan1S@users.noreply.github.com>
2025-07-01 14:58:07 +0530

    .

--- Arrays/Looping in arrays.tcl
+++ Arrays/Looping in arrays.tcl
@@ -1,3 +1,7 @@
+set institute(0) VLSI;
+set institute(1) Academy;
+set institute(2) India;
+
 for {set index 0} {$index < [array size institute]} {incr index} {
          puts "institute($index) : $institute($index)"
 }
