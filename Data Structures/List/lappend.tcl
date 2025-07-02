52af1b6306b36f28c7b0050bb8d3d66a1871e898
Irfaan <138297234+Irfaan1S@users.noreply.github.com>
2025-07-02 18:25:22 +0530

    Create lappend.tcl

---
+++ Data Structures/List/lappend.tcl
@@ -0,0 +1,8 @@
+set employee_list "Jacob Ryan Appu Radheshyam";
+set new_employee "shobhit";
+
+puts "Old database : $employee_list";
+
+lappend employee_list $new_employee;
+
+puts "updated database : $employee_list";
