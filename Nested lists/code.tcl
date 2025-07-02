0013889452304e877e6bbf4fa92ca32198c2a559
Irfaan <138297234+Irfaan1S@users.noreply.github.com>
2025-07-02 22:49:38 +0530

    ..

--- Nested lists/code.tcl
+++ Nested lists/code.tcl
@@ -3,5 +3,14 @@ set file_handle [open $filename r]
 set data {}
 
 while { [gets $file_handle line] >= 0 } {
-    puts "[lindex $line 3] and [lindex $line 4]"
+    #puts "[lindex $line 3] and [lindex $line 4]"
+
+    # appending unit of millivolts (mV) to unit1 value
+    set volts [join [list [lindex $line 3] mV] {}]
+
+    # appending unit of milliamperes (mA) to unit2 value
+    set current [join [list [lindex $line 4] nA] {}]
+
+    set line [lreplace $line 3 3  $volts]
+    set line [lreplace $line 4 4 $current]
 }
